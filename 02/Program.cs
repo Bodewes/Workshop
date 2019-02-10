001281c50c3d1808e142f4fbe85bdebaf0312967
Martijn Bodewes <bodewes@gmail.com>
2019-02-10 11:45:14 +0100

    Added 01 and 02, move first commit to 07.

---
+++ 02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello int!");
+
+            // Basis types: int, long, float, double, decimal, bool, char, string
+            // en nog meer: sbyte, short, byte, ushort, unit, ulong
+            int a = 42;
+            int b = 37;
+            var c = a + b;
+
+            Console.WriteLine("c = " + c);
+
+            /* Meer commentaar
+             * op meer regels
+             */
+
+            if (c > 50){
+                a = a + 100;
+            }else{
+                b *= 100;
+            }
+            Console.WriteLine("a = " + a + ", b = " + b);
+
+            // Loops: 
+            // for ( ;; ){}
+            // while( expressie ) { statements } 
+            for(int i = 0; i < 4; i++){
+                Console.WriteLine($"We tellen op: {i}!");
+            }
+            var j = 4;
+            while( j >= 0 ){
+                Console.WriteLine($"We tellen af: {j}!");
+                j--;
+            }
+
+            var s = "Workshop";
+            foreach(var c in s){
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+# C# Crash Course
+
+## 00 - Vooraf
+
+1. Installeer .net CORE SDK
+2. Installeer Visual Studio Code
+3. In VSCode: Installeer Extension **C# for Visual Studio Code (C# powerd by Omnisharp)**  (Ctrl+Shift+X)
+
+## 01 - Hello World
+
+1. Open VSCode
+2. Maak een nieuw Console app aan:  
+    `PS> dotnet new console`
+3. Daar je nieuwe Console app:  
+    `PS> dotnet run`
+4. Aleen compileren:  
+    `PS> dotnet build`
+
+## 02 - Hello int
+
+1. Maak een nieuw Console app aan:  
+    `PS> dotnet new console`
+2. Probeer verschillende combinaties van types uit.
+
+
+  
+## Rerenties
+
+- <https://docs.microsoft.com/en-us/dotnet/csharp/index>
+- <https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/>
